c86f7952923c6b5146cfda74e888274d98cbd564
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-08-13 17:08:47 +0000

    maj

--- src/rsh.py
+++ src/rsh.py
@@ -10,7 +10,23 @@ command_=""
 def main():
     global command_
     try:
-        command_ = input("r$ ")
+        # input from keyboard to command_ string, print the ask text ' R<--- ' in Green, Brown if command_ is empty
+        if command_ == "":
+            print("\033[92mR\033[0m\033[33m<---\033[0m ", end="")
+        else:
+            #display the text ' R<--- ' in Green, Brown and add an text '?' in Red
+            print("\033[92mR\033[0m\033[33m<---\033[0m \033[91m?\033[0m ", end="")
+        command_ = input()
+        # if command_ is empty, print the ask text ' R<--- ' in Green, Brown and return
+        if command_ == "":
+            print("\033[92mR\033[0m\033[33m<---\033[0m ", end="")
+            return
+        # if command_ is not empty, print the ask text ' R<--- ' in Green, Brown and execute the command_
+        else:
+            print("\033[92mR\033[0m\033[33m<---\033[0m\033[91m{}\033[0m ".format(command_), end="\n")
+            sh("-c",command_)
+            
+        
     except EOFError as e:
         command_ = None
 
