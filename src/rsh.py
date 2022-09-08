df6462deb5329ec6d3d78188edaf5e79ff8be1e7
0x07cb <83157348+0x07CB@users.noreply.github.com>
2022-09-08 08:35:30 +0200

    update

--- src/rsh.py
+++ src/rsh.py
@@ -4,8 +4,53 @@ from sh import Command as Cmd
 from sh import ifconfig
 from sh import hping3
 from multiprocessing import Process
-
+import socket ,requests
 command_=""
+
+# class to create an static http server
+class StaticServer:
+    def __init__(self, port=80, path='.', host='0.0.0.0'):
+        self.port = port
+        self.path = path
+        self.host = host
+        self.server = None
+        self.process = None
+    # function to set the port
+    def set_port(self, port):
+        self.port = port
+        return self
+    # function to set the path
+    def set_path(self, path):
+        self.path = path
+        return self
+    # function to set the host
+    def set_host(self, host):
+        self.host = host
+        return self
+    # function to start the server
+    def start(self,print_url=True):
+        self.process = Process(target=self._start)
+        self.process.start()
+        if print_url:
+            self.show_url()
+        return self
+    # function to stop the server
+    def stop(self):
+        self.process.terminate()
+        return self
+    # function to start the server
+    def _start(self):
+        self.server = Cmd('python3 -m http.server {} -d {}'.format(self.port, self.path))
+        self.server(_bg=True)
+        return self
+    # function to get the url
+    def get_url(self):
+        return 'http://{}:{}'.format(self.host, self.port)
+    # function named 'show_url' to display the url returned by 'self.get_url()'
+    def show_url(self):
+        print("The URL is: {}".format(self.get_url()))
+        return self
+
 
 def main():
     global command_
